ce38110ce7e168eccbbb63a3dd9707615bfc0628
Mohammed Anas <anas11champ@gmail.com>
2026-01-17 15:17:19 +0530

    Some change in FileRename.java

--- src/FileHandling/FileRename.java
+++ src/FileHandling/FileRename.java
@@ -1,4 +1,22 @@
 package FileHandling;
+import java.io.File;
+import java.io.IOException;
 
 public class FileRename {
+    public static void main(String[] args) throws IOException {
+        File f = new File("C:\\Users\\anas1\\Desktop\\data.txt");
+        File r = new File("C:\\Users\\anas1\\Desktop\\mydata.txt");
+
+        if(f.exists()){
+            System.out.println("File found...");
+
+            if(f.renameTo(r)){
+                System.out.println("File rename Successfully....");
+            } else {
+                System.out.println("File rename unsuccessful...!");
+            }
+        } else {
+            System.out.println("File not found....!");
+        }
+    }
 }
