64025948e93f69228a5d0114f4c0556843cb0f01
Mohammed Anas <anas11champ@gmail.com>
2026-01-19 14:49:22 +0530

    Practice today on Comparable interface

---
+++ src/DSAPractice/JCFPractice/ComparablePractice.java
@@ -0,0 +1,50 @@
+package DSAPractice.JCFPractice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Students implements Comparable<Students> {
+    int id;
+    String name;
+
+    Students(){
+        id = 0;
+        name = null;
+    }
+
+    Students(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString(){
+        return "Students [ id = " + id + ", Name = "+ name + " ]";
+    }
+
+
+    public int compareTo(Students that) {
+        if(this.id > that.id)
+            return 1;
+        else
+            return -1;
+    }
+}
+
+public class ComparablePractice {
+    public static void main(String[] args){
+        List<Students> ls = new ArrayList<>();
+        ls.add(new Students(2,"Anas"));
+        ls.add(new Students(7,"Anas"));
+        ls.add(new Students(3,"Anas"));
+        ls.add(new Students());
+        ls.add(new Students());
+        ls.add(new Students());
+
+        Collections.sort(ls);
+
+        for(Students s : ls){
+            System.out.println(s);
+        }
+    }
+}
